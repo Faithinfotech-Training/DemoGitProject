383208138299478ae189da30f3a021b83d3eb688
sahadan <sahadan@gmail.com>
2021-11-26 08:33:34 +0530

    Second commit

--- Startup.cs
+++ Startup.cs
@@ -25,6 +25,7 @@ namespace DemoGitProject
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            //Add service here
             services.AddControllers();
         }
 
